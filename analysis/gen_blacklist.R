0a64a41e4920d61324ea585ffcf461e02405b085
daniel-wells <daniel-wells@users.noreply.github.com>
2019-01-17 17:22:35 +0000

    generify gen_blacklist

--- analysis/gen_blacklist.R
+++ analysis/gen_blacklist.R
@@ -1,40 +1,56 @@
 
 args <- commandArgs(TRUE)
 
-print(str(args))
+str(args)
 
-bedloc <- args[1]
-id <- args[2]
-output_dir <- args[3]
+input <- args[1]
+output <- args[2]
 
 library(data.table)
 library(ggplot2)
-cov_ic <- fread(paste0("zcat < ",bedloc,id,"_indexcov-indexcov.bed.gz"))
+cov_ic <- fread(paste0("zcat < ",input))
 names(cov_ic) <- make.names(names(cov_ic))
 
 names(cov_ic)[4] <- "sample"
 
-pdf(file = paste0(output_dir,id,"_coverage.pdf"))
+cov_ic <- cov_ic[X.chrom!="chrM"] # only one data point
 
-#[X.chrom %in% paste0("chr",1:22) & start!=0]
+pdf(file = paste0(output,"_coverage.pdf"))
+
 ggplot(cov_ic, aes(start, sample)) +
         geom_line() +
         scale_y_sqrt() +
+		geom_hline(yintercept=quantile(cov_ic$sample, 0.9999), colour='red') +
         facet_wrap(~X.chrom, scales="free_x")
 
 ggplot(cov_ic, aes(start, sample)) +
-        geom_line() +
-        coord_cartesian(ylim=c(0,1000)) +
-        facet_wrap(~X.chrom, scales="free_x")
+	geom_line() +
+	coord_cartesian(ylim=c(0,max(cov_ic$sample))) +
+	geom_hline(yintercept=quantile(cov_ic$sample, 0.9999), colour='red') +
+	facet_wrap(~X.chrom, scales="free_x")
 
-ggplot(cov_ic[X.chrom %in% paste0("chr",10) & start!=0], aes(start, sample)) +
+ggplot(cov_ic, aes(start, sample)) +
+	geom_line() +
+	coord_cartesian(ylim=c(0,quantile(cov_ic$sample, 0.99999))) +
+	geom_hline(yintercept=quantile(cov_ic$sample, 0.9999), colour='red') +
+	facet_wrap(~X.chrom, scales="free_x")
+
+ggplot(cov_ic, aes(start, sample)) +
+	geom_line() +
+	coord_cartesian(ylim=c(0,quantile(cov_ic$sample, 0.9999))) +
+	facet_wrap(~X.chrom, scales="free_x")
+
+ggplot(cov_ic[X.chrom %in% paste0("chr",1) & start!=0], aes(start, sample)) +
         geom_line() +
-        coord_cartesian(ylim=c(0,1000))
+		geom_hline(yintercept=quantile(cov_ic$sample, 0.9999), colour='red') +
+        coord_cartesian(ylim=c(0,max(cov_ic[X.chrom %in% paste0("chr",1) & start!=0]$sample)))
 dev.off()
 
-write.table(cov_ic[sample>10][,.(X.chrom, start, end)],
+print(paste("Using blacklist threshold value of", quantile(cov_ic$sample, 0.9999)))
+
+write.table(cov_ic[sample>quantile(cov_ic$sample, 0.9999)][,.(X.chrom, start, end)],
             quote = F,
             row.names = F,
             col.names = F,
             sep = "\t",
-            file = paste0(id,"_blacklist.bed"))
+            file = output)
