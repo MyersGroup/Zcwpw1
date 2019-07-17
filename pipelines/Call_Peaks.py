aaaa5be7d4207f2905f0392923ff92a1645aa59a
daniel-wells <daniel-wells@users.noreply.github.com>
2019-07-17 11:29:17 +0100

    Update Call_Peaks.py to use sample_pairings file

--- pipelines/Call_Peaks.py
+++ pipelines/Call_Peaks.py
@@ -12,68 +12,26 @@
 # > Fragment_Position_${sample}.sorted.bed
 
 
+configfile: "pipelines/config.yml"
+include: "sample_pairings.py"
 
 from os.path import join
 METADATA_DIR = config["metadata_dir"]
 GENOME = 'hg38'
 
-# chip, control
-# when there are two biological replicates, seperate them using the special string '_AND_'
-# when there are pseudoreplicates, just enter the common sample name, assumes the PR1 PR2 file ending convention was used
 
-comparisons = [("WTCHG_538916_221156", "WTCHG_538916_217108"),  #Zcw only Chip vs Input
-              ("WTCHG_538916_223180", "WTCHG_538916_220144"), #Cotransfected Chip vs Cotransfected Input
-              ("WTCHG_538916_223180", "WTCHG_538916_217108_AND_WTCHG_538916_220144"), #Cotransfected Chip vs Combined Input
-
-              ("WTCHG_538916_223180", "WTCHG_538916_221156"),  #Chip with Prdm9 vs Chip
-              ("WTCHG_538916_224192", "WTCHG_538916_221156"), # chimpPrdm9+Zcw vs chip
-              ("WTCHG_538916_224192", "WTCHG_538916_217108_AND_WTCHG_538916_220144"), # chimpPrdm9+Zcw vs Combined hInput
-
-              # Human
-              ("SRA_Altemose2015_SRR5627138_AND_SRA_Altemose2015_SRR5627139", "SRA_Altemose2015_SRR5627140"), # hPrdm9-Nterm vs YFP_HumanPRDM9.Input
-
-              ("SRA_Altemose2015_SRR5627141", "SRA_Altemose2015_SRR5627140"), # YFP_HumanPRDM9.antiH3K4me3 vs YFP_HumanPRDM9.Input
-              #("SRA_Altemose2015_SRR5627141", "SRA_Altemose2015_SRR5627136_AND_SRA_Altemose2015_SRR5627135"), # YFP_HumanPRDM9.antiH3K4me3 vs Untransfected.antiH3K4me3
-
-              ("SRA_Altemose2015_SRR5627146", "SRA_Altemose2015_SRR5627143"), # hPrdm9 HA vs Input
-              ("SRA_Altemose2015_SRR5627147", "SRA_Altemose2015_SRR5627143"), # hPrdm9 V5 vs Input (HA)
-              ("SRA_Altemose2015_SRR5627146_AND_SRA_Altemose2015_SRR5627147", "SRA_Altemose2015_SRR5627143"), # hPrdm9-Cterm HA & V5 vs Input (HA)
-
-              # Chimp
-              ("SRA_Altemose2015_SRR5627145", "SRA_Altemose2015_SRR5627143"), # cPrdm9 V5 vs Input (HA)
-              ("SRA_Altemose2015_SRR5627144", "SRA_Altemose2015_SRR5627143"), # cPrdm9 HA vs (h) Input
-              ("SRA_Altemose2015_SRR5627145_AND_SRA_Altemose2015_SRR5627144", "SRA_Altemose2015_SRR5627143"), # cPrdm9 V5 & HA vs Input (HA)
-
-              # Histone Mods
-              ("SRA_Altemose2015_SRR5627152_AND_SRA_Altemose2015_SRR5627153", "SRA_Altemose2015_SRR5627143"), # H3K4 peaks wPrdm9
-              ("SRA_Altemose2015_SRR5627149", "SRA_Altemose2015_SRR5627143"), # H3K36 peaks wPprdm9
-              ("SRA_Altemose2015_SRR5627150_AND_SRA_Altemose2015_SRR5627151", "SRA_Altemose2015_SRR5627142"), # untransfected H3K4 peaks
-              ("SRA_Altemose2015_SRR5627148", "SRA_Altemose2015_SRR5627142"), # untransfected H3K36 peaks
-
-
-              # Chip H3K4 & ZF only
-              ("WTCHG_659233_256196", "WTCHG_659233_255184"), # ChIPV5_hPRDM9-ZFonly-V5 vs input
-
-              ("WTCHG_659233_253160", "WTCHG_659233_252148"), # ChIPHA_ZCWPW1-HA+cPRDM9-V5 vs input
-              ("WTCHG_659233_254172", "WTCHG_659233_252148"), # ChIPH3K4me3_ZCWPW1-HA+cPRDM9-V5 vs input
-
-              ("WTCHG_659233_250124", "WTCHG_659233_249112"), # ChIPHA_ZCWPW1-HA+hPRDM9-V5 vs input
-              ("WTCHG_659233_251136", "WTCHG_659233_249112"), # ChIPH3K4me3_ZCWPW1-HA+hPRDM9-V5 vs input
-
-              ("WTCHG_659233_234122", "WTCHG_659233_233110"), # ChIPHA_ZCWPW1-HA+hPRDM9-ZFonly-V5 vs input
-              ("WTCHG_659233_235134", "WTCHG_659233_233110")] # ChIPH3K4me3_ZCWPW1-HA+hPRDM9-ZFonly-V5 vs input
 
 rule all:
   input:
     ["peaks/SingleBasePeaks.{chip}_vs_{control}.p0.000001.sep250.ALL.bed".format(
         chip = chip_id,
-        control=control_id) for chip_id, control_id in comparisons],
+        control=control_id) for chip_id, control_id, name in sample_pairings],
     ["deeptools/bigwigs/{chip}_vs_{control}_CPM_log2.bw".format(
         chip = chip_id,
-        control=control_id) for chip_id, control_id in comparisons]
+        control=control_id) for chip_id, control_id, name in sample_pairings]
     #["deeptools/bigwigs/SBPsample/{chip}_vs_{control}_CPM_log2.bw".format(
     #    chip = chip_id,
-    #    control=control_id) for chip_id, control_id in comparisons]
+    #    control=control_id) for chip_id, control_id in sample_pairings]
 
 
 def get_chips(wildcards):
