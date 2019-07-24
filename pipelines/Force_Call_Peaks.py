fdd18f261ec31d69886b412ba695b2eb4abbf4c8
daniel-wells <daniel-wells@users.noreply.github.com>
2019-07-24 21:21:55 +0100

    Update Force_Call_Peaks to use config.yml

--- pipelines/Force_Call_Peaks.py
+++ pipelines/Force_Call_Peaks.py
@@ -1,60 +1,24 @@
-# snakemake --cores 15 --snakefile Snakefile_forcecall -npr
+# snakemake --cores 15 --snakefile pipelines/Force_Call_Peaks.py -npr
+
+configfile: "pipelines/config.yml"
+include: "sample_pairings.py"
+
 
 from os.path import join
 METADATA_DIR = config["metadata_dir"]
 GENOME = 'hg38'
 
-comparisons = [("WTCHG_538916_221156", "WTCHG_538916_217108"),  #Zcw only Chip vs Input
-              ("WTCHG_538916_223180", "WTCHG_538916_220144"), #Cotransfected Chip vs Cotransfected Input
-              ("WTCHG_538916_223180", "WTCHG_538916_221156"),  #Chip vs Chip with Prdm9
-              ("WTCHG_538916_224192", "WTCHG_538916_221156"), # vs chimpPrdm9
-              ("SRA_Altemose2015_SRR5627146", "SRA_Altemose2015_SRR5627143"), # hPrdm9 vs Input
-              ("SRA_Altemose2015_SRR5627147", "SRA_Altemose2015_SRR5627143"), # hPrdm9 V5 vs Input (HA)
-              ("SRA_Altemose2015_SRR5627146_AND_SRA_Altemose2015_SRR5627147", "SRA_Altemose2015_SRR5627143"), # hPrdm9 HA & V5 vs Input (HA)
-              ("SRA_Altemose2015_SRR5627144", "SRA_Altemose2015_SRR5627143"), # cPrdm9 vs (h) Input
-              ("SRA_Altemose2015_SRR5627152_AND_SRA_Altemose2015_SRR5627153", "SRA_Altemose2015_SRR5627143"), #SRR5627153 ! # H3K4 peaks wPrdm9
-              ("SRA_Altemose2015_SRR5627149", "SRA_Altemose2015_SRR5627143"), # H3K36 peaks wPprdm9
-              ("SRA_Altemose2015_SRR5627150", "SRA_Altemose2015_SRR5627142"), # untransfected H3K4 peaks
-              ("SRA_Altemose2015_SRR5627148", "SRA_Altemose2015_SRR5627142")] # untransfected H3K36 peaks
+# override sample_pairings to compute
+# sample_pairings = [("SRA_Altemose2015_SRR5627138_AND_SRA_Altemose2015_SRR5627139", "SRA_Altemose2015_SRR5627140"),# YFP Nterm
+#                 ("SRA_Altemose2015_SRR5627141", "SRA_Altemose2015_SRR5627140")] # YFP Nterm H3K4me3
 
-comparisons = [("WTCHG_538916_221156", "WTCHG_538916_217108"),  #Zcw only Chip vs Input
-              ("WTCHG_538916_223180", "WTCHG_538916_220144"), #Cotransfected Chip vs Cotransfected Input
-
-
-              ("WTCHG_538916_223180", "WTCHG_538916_221156"),  #Chip with Prdm9 vs Chip
-              ("WTCHG_538916_224192", "WTCHG_538916_221156"), # chimpPrdm9 vs chip
-
-              # Human
-              ("SRA_Altemose2015_SRR5627138_AND_SRA_Altemose2015_SRR5627139", "SRA_Altemose2015_SRR5627140"), # hPrdm9-Nterm vs Input
-              ("SRA_Altemose2015_SRR5627146", "SRA_Altemose2015_SRR5627143"), # hPrdm9 HA vs Input
-              ("SRA_Altemose2015_SRR5627147", "SRA_Altemose2015_SRR5627143"), # hPrdm9 V5 vs Input (HA)
-              ("SRA_Altemose2015_SRR5627146_AND_SRA_Altemose2015_SRR5627147", "SRA_Altemose2015_SRR5627143"), # hPrdm9-Cterm HA & V5 vs Input (HA)
-
-              # Chimp
-              ("SRA_Altemose2015_SRR5627145", "SRA_Altemose2015_SRR5627143"), # cPrdm9 V5 vs Input (HA)
-              ("SRA_Altemose2015_SRR5627144", "SRA_Altemose2015_SRR5627143"), # cPrdm9 HA vs (h) Input
-              ("SRA_Altemose2015_SRR5627145_AND_SRA_Altemose2015_SRR5627144", "SRA_Altemose2015_SRR5627143"), # cPrdm9 V5 & HA vs Input (HA)
-
-              # Histone Mods
-              ("SRA_Altemose2015_SRR5627152_AND_SRA_Altemose2015_SRR5627153", "SRA_Altemose2015_SRR5627143"), # H3K4 peaks wPrdm9
-              ("SRA_Altemose2015_SRR5627149", "SRA_Altemose2015_SRR5627143"), # H3K36 peaks wPprdm9
-              ("SRA_Altemose2015_SRR5627150", "SRA_Altemose2015_SRR5627142"), # untransfected H3K4 peaks
-              ("SRA_Altemose2015_SRR5627148", "SRA_Altemose2015_SRR5627142")] # untransfected H3K36 peaks
-
-comparisons = [("SRA_Altemose2015_SRR5627138_AND_SRA_Altemose2015_SRR5627139", "SRA_Altemose2015_SRR5627140"),# YFP Nterm
-                ("SRA_Altemose2015_SRR5627141", "SRA_Altemose2015_SRR5627140")] # YFP Nterm H3K4me3
-
-forcecallAT = "SRA_Altemose2015_SRR5627138_AND_SRR5627139_vs_SRA_Altemose2015_SRR5627140"
-forcecallAT = "SRA_Altemose2015_SRR5627146_AND_SRA_Altemose2015_SRR5627147_vs_SRA_Altemose2015_SRR5627143"
-forcecallAT = "SRA_Altemose2015_SRR5627138_AND_SRA_Altemose2015_SRR5627139_vs_SRA_Altemose2015_SRR5627140.flank150"
-forcecallAT = "genome.windows.100wide.100slide.bed"
-forcecallAT = "qPCR_regions_for_validation.bed"
+forcecallAT = config["forcecallAT"]
 
 rule all:
   input:
     ["peaks/ForceCalledPeaks_{chip}_vs_{control}_AT_{at}.bed".format(
         chip = chip_id,
-        control = control_id, at=forcecallAT) for chip_id, control_id in comparisons]
+        control = control_id, at=forcecallAT) for chip_id, control_id, name in sample_pairings]
 
 
 def get_chips(wildcards):
